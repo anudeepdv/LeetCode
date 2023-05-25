0519f297666680beb431884cca2b67ee29f3fdad
Anudeep D V <anudeepdv333@gmail.com>
2023-05-25 01:34:43 -0700

    Time: 52 ms (17.24%) | Memory: 16.2 MB (41.37%) - LeetSync

---
+++ reverse-bits/reverse-bits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        
+        res=0
+        for i in range(32):
+            res = res<<1
+            res=res + n%2
+
+            n=n>>1
+
+        return res
