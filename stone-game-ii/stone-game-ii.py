6c92167e06c728aaeb1162265dc9366e2f74243b
Anudeep D V <anudeepdv333@gmail.com>
2023-05-26 14:06:35 -0700

    Time: 291 ms (70.75%) | Memory: 17.6 MB (51.89%) - LeetSync

--- stone-game-ii/stone-game-ii.py
+++ stone-game-ii/stone-game-ii.py
@@ -6,22 +6,21 @@ class Solution:
         def recursiveStoneGame(start, M):            
             if start >= N:
                 return 0
-            
-            # take all if possible
+           
             if N - start <= 2*M:
                 return sum(piles[start:])
             
-            # memoization
+        
             if (start, M) in self.dp:
                 return self.dp[(start, M)]
 
             my_score = 0
             total_score = sum(piles[start:])
-            # the opponent can take [1, 2*M] stones
+         
             for x in range(1, 2*M+1):
-                # get opponent's score
+         
                 opponent_score = recursiveStoneGame(start+x, max(x, M))
-                # maintains max my_score
+          
                 my_score = max(my_score, total_score - opponent_score)
                 
             self.dp[(start, M)] = my_score
