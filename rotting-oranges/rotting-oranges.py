a13143f13775d470e30f88cf1a4a0249f4290fa5
Anudeep D V <anudeepdv333@gmail.com>
2023-06-04 11:48:07 -0700

    Time: 78 ms (9.26%) | Memory: 16.2 MB (58.07%) - LeetSync

---
+++ rotting-oranges/rotting-oranges.py
@@ -0,0 +1,55 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+
+        rows =len(grid)
+        cols=len(grid[0])
+
+        fresh=0
+        q=collections.deque()
+        visited=set()
+        initialRotten=0
+
+        for r in range(rows):
+            for c in range(cols):
+
+                if grid[r][c]==1:
+                    fresh=fresh+1
+                elif grid[r][c]==2:
+                    q.append((r,c))
+                    visited.add((r,c))
+                    initialRotten+=1
+
+        if initialRotten==0 and fresh==0:
+            return 0
+        directions = [[1,0],[0,1],[-1,0],[0,-1]]
+
+        cal=0
+        time=0
+        while q:
+
+
+            for i in range(len(q)):
+                x,y = q.popleft()
+
+                for l,r in directions:
+                    nx,ny= x+l,y+r
+
+                    if nx in range(rows) and ny in range(cols) and grid[nx][ny]==1 and (nx,ny) not in visited :
+                        q.append((nx,ny))
+                        visited.add((nx,ny))
+                        cal=cal+1
+
+            time=time+1
+
+        if cal==fresh:
+            return time-1
+        return -1
+
+        
+
+
+
+
+
+        
+
