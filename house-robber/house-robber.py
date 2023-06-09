14a2f80ce20ae555a71a6e9fc16f60b48f266093
Anudeep D V <anudeepdv333@gmail.com>
2023-06-08 23:54:41 -0700

    Time: 53 ms (17.16%) | Memory: 16.3 MB (23.09%) - LeetSync

--- house-robber/house-robber.py
+++ house-robber/house-robber.py
@@ -10,4 +10,5 @@ class Solution:
             y = 0 if i-3<0 else dp[i-3]
             dp[i]=max(nums[i]+x,nums[i]+y)
 
-        return max(dp)
+        print(dp)
+        return max(dp[n-1],dp[n-2])
