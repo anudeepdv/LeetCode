3b46972a96145a317f450777e20a3fb2b41d61d8
Anudeep D V <anudeepdv333@gmail.com>
2023-06-08 23:36:55 -0700

    Time: 38 ms (74.31%) | Memory: 16.3 MB (23.09%) - LeetSync

---
+++ house-robber/house-robber.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n=len(nums)
+        dp = [0 for i in range(n)]
+
+        dp[0]=nums[0]
+
+        for i in range(1,n):
+            x = 0 if i-2<0 else dp[i-2]
+            y = 0 if i-3<0 else dp[i-3]
+            dp[i]=max(nums[i]+x,nums[i]+y)
+
+        return max(dp)
