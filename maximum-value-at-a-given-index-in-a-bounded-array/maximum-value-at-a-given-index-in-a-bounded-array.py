a0cc36fd4c4b458add453f858d81c98225791d88
Anudeep D V <anudeepdv333@gmail.com>
2023-06-10 12:03:00 -0700

    Time: 76 ms (7.63%) | Memory: 16.5 MB (6.11%) - LeetSync

--- maximum-value-at-a-given-index-in-a-bounded-array/maximum-value-at-a-given-index-in-a-bounded-array.py
+++ maximum-value-at-a-given-index-in-a-bounded-array/maximum-value-at-a-given-index-in-a-bounded-array.py
@@ -16,20 +16,19 @@ class Solution:
             m=mid-1
             rsum=0
             lsum=0
-           
+            if rr!=0:
 
-            if  rr<=m:
-                rsum = (m*(m+1)//2) - (m-rr)*(m-rr+1)//2
-            else:
-                rsum = (m*(m+1)//2) +(rr-m)*1
+                if  rr>=m:
+                    rsum = (m*(m+1)//2) +(rr-m)*1
+                else:
+                    rsum = (m*(m+1)//2) - (m-rr)*(m-rr+1)//2
 
-          
+            if ll!=0:
 
-            if  ll<=m:
-                lsum = (m*(m+1)//2) - (m-ll)*(m-ll+1)//2
-            else:
-               
-                lsum = (m*(m+1)//2) +(ll-m)*1
+                if  ll>=m:
+                    lsum = (m*(m+1)//2) +(ll-m)*1
+                else:
+                    lsum = (m*(m+1)//2) - (m-ll)*(m-ll+1)//2
 
             curSum+=lsum+rsum
             print(rsum,lsum,mid,curSum,maxSum)
