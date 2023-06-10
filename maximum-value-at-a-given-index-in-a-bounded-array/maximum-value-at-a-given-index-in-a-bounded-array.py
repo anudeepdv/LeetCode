d3a665f767d1a5696da0c433059663ad2be5c1a9
Anudeep D V <anudeepdv333@gmail.com>
2023-06-10 11:58:28 -0700

    Time: 71 ms (9.16%) | Memory: 16.5 MB (6.11%) - LeetSync

---
+++ maximum-value-at-a-given-index-in-a-bounded-array/maximum-value-at-a-given-index-in-a-bounded-array.py
@@ -0,0 +1,47 @@
+class Solution:
+    def maxValue(self, n: int, index: int, maxSum: int) -> int:
+
+        lo = 1
+        high = maxSum
+
+        rr = n-index-1
+        ll=index
+        res=0
+        while (lo<=high):
+
+            mid = (lo+high)//2
+
+            curSum = mid
+
+            m=mid-1
+            rsum=0
+            lsum=0
+           
+
+            if  rr<=m:
+                rsum = (m*(m+1)//2) - (m-rr)*(m-rr+1)//2
+               
+            else:
+                 rsum = (m*(m+1)//2) +(rr-m)*1
+
+
+            if  ll<=m:
+                lsum = (m*(m+1)//2) - (m-ll)*(m-ll+1)//2
+            else:
+                lsum = (m*(m+1)//2) +(ll-m)*1
+                
+
+            curSum+=lsum+rsum
+            print(rsum,lsum,mid,curSum,maxSum)
+            if curSum<=maxSum:
+                res=max(res,mid)
+                lo=mid+1
+            else:
+                high=mid-1
+
+
+        return res
+
+
+            
+
