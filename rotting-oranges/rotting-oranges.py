8dfeff54c00b20c0f13d8f8b3b93232c5521800d
Anudeep D V <anudeepdv333@gmail.com>
2023-06-11 17:16:19 -0700

    Time: 79 ms (9.44%) | Memory: 16.5 MB (16.23%) - LeetSync

--- rotting-oranges/rotting-oranges.py
+++ rotting-oranges/rotting-oranges.py
@@ -1,55 +1,48 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-
-        rows =len(grid)
-        cols=len(grid[0])
-
+        
+        m=len(grid)
+        n=len(grid[0])
         fresh=0
         q=collections.deque()
         visited=set()
-        initialRotten=0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]==2:
+                    q.append((i,j))
+                    visited.add((i,j))
 
-        for r in range(rows):
-            for c in range(cols):
+                elif grid[i][j]==1:
+                    fresh+=1
 
-                if grid[r][c]==1:
-                    fresh=fresh+1
-                elif grid[r][c]==2:
-                    q.append((r,c))
-                    visited.add((r,c))
-                    initialRotten+=1
-
-        # if initialRotten==0 and fresh==0:
-        #     return 0
+        time=1
+        if fresh==0:
+            return 0
         directions = [[1,0],[0,1],[-1,0],[0,-1]]
-
-        cal=0
-        time=0
-        while q and cal!=fresh:
-
+        while q:
 
             for i in range(len(q)):
-                x,y = q.popleft()
 
-                for l,r in directions:
-                    nx,ny= x+l,y+r
+                ox,oy = q.popleft()
 
-                    if nx in range(rows) and ny in range(cols) and grid[nx][ny]==1 and (nx,ny) not in visited :
+                for x,y in directions:
+
+                    nx,ny = ox+x,oy+y
+
+                    if nx in range(m) and ny in range(n) and grid[nx][ny]==1 and (nx,ny) not in visited:
                         q.append((nx,ny))
                         visited.add((nx,ny))
-                        cal=cal+1
+                        fresh=fresh-1
 
-            time=time+1
+                        if fresh==0:
+                            return time
 
-        if cal==fresh:
-            return time
+
+
+            time+=1
+
         return -1
 
-        
 
 
-
-
-
-        
 
