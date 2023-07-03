806f9b4423f3d876e37219f5f79b714797370a73
Anudeep D V <anudeepdv333@gmail.com>
2023-07-03 14:12:16 -0700

    Time: 1216 ms (65.18%) | Memory: 17 MB (12.74%) - LeetSync

--- maximum-number-of-achievable-transfer-requests/maximum-number-of-achievable-transfer-requests.py
+++ maximum-number-of-achievable-transfer-requests/maximum-number-of-achievable-transfer-requests.py
@@ -1,26 +1,32 @@
 class Solution:
-    def __init__(self):
-        self.ans = 0
+    def maximumRequests(self, n: int, requests: List[List[int]]) -> int:
+        
+        bc=[0]*n
+        ans=[]
+        def dfs(i,count):
 
-    def helper(self, start, requests, indegree, n, count):
-        if start == len(requests):
-            for i in range(n):
-                if indegree[i] != 0:
-                    return
-            self.ans = max(self.ans, count)
-            return
+            if i==len(requests):            
+                for i in bc:
+                    if i!=0:
+                        return
+                ans.append(count)
+                return
+            
+            bc[requests[i][0]]=bc[requests[i][0]]-1
+            bc[requests[i][1]]=bc[requests[i][1]]+1
 
-        # Take 
-        indegree[requests[start][0]] -= 1
-        indegree[requests[start][1]] += 1
-        self.helper(start + 1, requests, indegree, n, count + 1)
+            dfs(i+1,count+1)
 
-        # Not-take
-        indegree[requests[start][0]] += 1
-        indegree[requests[start][1]] -= 1
-        self.helper(start + 1, requests, indegree, n, count)
+            bc[requests[i][0]]=bc[requests[i][0]]+1
+            bc[requests[i][1]]=bc[requests[i][1]]-1
 
-    def maximumRequests(self, n, requests):
-        indegree = [0] * n
-        self.helper(0, requests, indegree, n, 0)
-        return self.ans
+            dfs(i+1,count)
+
+        dfs(0,0)
+
+        return max(ans)
+
+
+
+
+
