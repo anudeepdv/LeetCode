1324e1e6b9230a01e0f33adcaad11fe94315cd77
Anudeep D V <anudeepdv333@gmail.com>
2023-07-04 12:37:09 -0700

    Time: 108 ms (88.99%) | Memory: 19.2 MB (41.00%) - LeetSync

--- course-schedule/course-schedule.py
+++ course-schedule/course-schedule.py
@@ -1,36 +1,34 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        adjList = collections.defaultdict(list)
 
-        adjList=collections.defaultdict(list)
+        for s,d in  prerequisites:
+            adjList[s].append(d)
 
-        for s,d in prerequisites:
-            adjList[d].append(s)
+        visisted={}
 
-        visitedMap={}
         for i in range(numCourses):
-            visitedMap[i]=0
-
+            visisted[i]=0
 
         def isCycle(node):
 
-            if visitedMap[node]==2:
+            if visisted[node]==2:
                 return True
-            
-            visitedMap[node]=2
+
+            visisted[node]=2
 
             for nei in adjList[node]:
-                if visitedMap[nei]!=1:
+                if visisted[nei]!=1:
                     if isCycle(nei):
                         return True
-            visitedMap[node]=1
 
+            visisted[node]=1
             return False
 
         for i in range(numCourses):
-            if visitedMap[i]==0:
+            if visisted[i]==0:
                 if isCycle(i):
                     return False
 
         return True
-
-        
