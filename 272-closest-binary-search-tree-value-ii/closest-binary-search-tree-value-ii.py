9a19fe4874f49fd240c1c77f8542fabff6734e21
Anudeep D V <anudeepdv333@gmail.com>
2023-07-19 13:07:35 -0700

    Time: 89 ms (5.42%) | Memory: 19.2 MB (14.16%) - LeetSync

---
+++ 272-closest-binary-search-tree-value-ii/closest-binary-search-tree-value-ii.py
@@ -0,0 +1,40 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def closestKValues(self, root: Optional[TreeNode], target: float, k: int) -> List[int]:
+
+        res=float('inf')
+        val=[]
+        q=collections.deque()
+        q.append(root)
+
+        h=[]
+        while q:
+            for _ in range(len(q)):
+                pop = q.popleft()
+                print(pop.val,abs(pop.val-target),target,res)
+                h.append((abs(pop.val-target),pop.val))
+                if abs(pop.val-target)<res:
+                    res=abs(pop.val-target)
+                    val=pop.val
+                if abs(pop.val-target)==res:
+                    val=min(pop.val,val)
+
+                if pop.left:
+                    q.append(pop.left)
+
+                if pop.right:
+                    q.append(pop.right)
+       
+       
+        heapq.heapify(h)
+        r=[]
+
+        for i in range(k):
+            r.append(heapq.heappop(h)[1])
+
+        return r
