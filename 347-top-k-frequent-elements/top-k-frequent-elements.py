d5060f652e599ad952be31c3d63b60162cb33496
Anudeep D V <anudeepdv333@gmail.com>
2024-02-05 04:14:10 +0530

    Time: 102 ms (27.29%) | Memory: 27 MB (5.05%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,19 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        d=collections.defaultdict(int)
+
+        for i in nums:
+            d[i]+=1
+
+        s=collections.defaultdict(list)
+        for key,val in d.items():
+            s[val].append(key)
+
+        res=[]
+        for i in range(len(nums),-1,-1):
+            print(i)
+            for j in s[i]:
+                res.append(j)
+                if len(res)==k:
+                    return res
