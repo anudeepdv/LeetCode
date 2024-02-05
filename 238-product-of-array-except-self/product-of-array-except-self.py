ddca9efce2168f457eb9c52d654c92080ea68ac2
Anudeep D V <anudeepdv333@gmail.com>
2024-02-05 23:58:14 +0530

    Time: 175 ms (47.82%) | Memory: 24.9 MB (30.68%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -6,7 +6,7 @@ class Solution:
         for i in range(len(nums)-1):
             n1[k]=n1[k-1]*nums[i]
             k=k+1
-        print(n1)
+        # print(n1)
 
         n2=[1 for i in range(len(nums))]
         k=len(nums)-2
@@ -14,7 +14,7 @@ class Solution:
             n2[k]=n2[k+1]*nums[i]
             k=k-1
 
-        print(n2)
+        # print(n2)
 
         n3=[1 for i in range(len(nums))]
 
