9865ccef11c241e9494fbb1e20db2e28c5a16885
Anudeep D V <anudeepdv333@gmail.com>
2024-02-05 23:58:09 +0530

    Time: 196 ms (25.83%) | Memory: 25.1 MB (23.76%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -0,0 +1,24 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        n1=[1 for i in range(len(nums))]
+        k=1
+        for i in range(len(nums)-1):
+            n1[k]=n1[k-1]*nums[i]
+            k=k+1
+        print(n1)
+
+        n2=[1 for i in range(len(nums))]
+        k=len(nums)-2
+        for i in range(len(nums)-1,0,-1):
+            n2[k]=n2[k+1]*nums[i]
+            k=k-1
+
+        print(n2)
+
+        n3=[1 for i in range(len(nums))]
+
+        for i in range(len(nums)):
+            n3[i]=n1[i]*n2[i]
+
+        return n3
