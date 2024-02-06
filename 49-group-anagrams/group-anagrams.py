ff1ada5cd68f55e830808a31884028de1e741d15
Anudeep D V <anudeepdv333@gmail.com>
2024-02-06 07:40:05 +0530

    Time: 106 ms (24.88%) | Memory: 22.2 MB (25.15%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,15 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        s=collections.defaultdict(list)
 
-        print(ord("a"))
-        print(ord("z"))
-        d=collections.defaultdict(list)
         for i in strs:
-            l=[0]*26
-            for a in i:
-                l[ord(a)-97]+=1
-            key= tuple(l)
-            d[key].append(i)
 
-        return d.values()
-            
+            n=[0 for i in range(26)]
+            for ch in i:
+                n[ord(ch)-97]+=1
+            s[tuple(n)].append(i)
+
+        return s.values()
+        
+
+
+        
