79df94ece23865ce77fa3f49822660988b2a9f2f
Anudeep D V <anudeepdv333@gmail.com>
2024-02-12 11:33:31 -0800

    Time: 192 ms (48.64%) | Memory: 23.1 MB (26.78%) - LeetSync

---
+++ 816-design-hashset/design-hashset.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self,value):
+        self.val=value
+        self.next=None
+
+
+class MyHashSet:
+
+    def __init__(self):  
+        self.keyStore= [Node(-1) for i in range(10**4)]
+
+    def add(self, key: int) -> None:
+        hashed = key%len(self.keyStore)
+        cur = self.keyStore[hashed]  
+        while cur.next:
+            if cur.next.val ==key:
+                return
+            cur=cur.next      
+        cur.next = Node(key)
+
+    def remove(self, key: int) -> None:
+        hashed = key%len(self.keyStore)
+        cur = self.keyStore[hashed]
+        while cur.next:
+            if cur.next.val ==key:
+                cur.next=cur.next.next
+                return
+            cur=cur.next
+  
+    def contains(self, key: int) -> bool:
+        hashed = key%len(self.keyStore)
+        cur = self.keyStore[hashed]
+        while cur.next:
+            if cur.next.val ==key:
+                return True
+            cur=cur.next
+        return False
+        
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
