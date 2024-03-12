5e8f651fa142fe1a3e5ccf968f5ce2a1e611f1ce
Anudeep D V <anudeepdv333@gmail.com>
2024-03-12 12:16:07 -0700

    Time: 35 ms (93.63%) | Memory: 16.9 MB (29.48%) - LeetSync

--- 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.py
+++ 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.py
@@ -1,29 +1,33 @@
+# Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def removeZeroSumSublists(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode(0)
-        dummy.next = head
-        prefix_sum = 0
-        prefix_sum_map = {}
         
-        current = dummy
-        
-        while current:
-            prefix_sum += current.val
-            prefix_sum_map[prefix_sum] = current
-            current = current.next
-        
-        current = dummy
-        prefix_sum = 0
-        
-        while current:
-            prefix_sum += current.val
-            if prefix_sum in prefix_sum_map:
-                current.next = prefix_sum_map[prefix_sum].next
-            current = current.next
-        
+        dummy= ListNode(0,head)
+
+        pr_sum=0
+        pr_map={}
+
+        cur=dummy.next
+
+        while cur:
+            pr_sum+=cur.val
+            pr_map[pr_sum]=cur
+            cur=cur.next
+
+
+
+        cur = dummy
+        pr_sum=0
+
+        while cur:
+            pr_sum+=cur.val
+            if pr_sum in pr_map:
+                cur.next = pr_map[pr_sum].next
+
+            cur=cur.next
         return dummy.next
+
