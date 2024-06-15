289c0cd74334fe1e4892c07e34a363d484b8e4bf
Anudeep D V <anudeepdv333@gmail.com>
2024-06-15 22:33:28 +0530

    Time: 81 ms (95.98%) | Memory: 16.6 MB (50.97%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,21 +1,23 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         
-        r=collections.defaultdict(set)
-        c=collections.defaultdict(set)
-        b=collections.defaultdict(set)
-        for i in range(len(board)):
-            for j in range(len(board)):
-                if board[i][j]=='.':
+        row = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        box = collections.defaultdict(set)
+
+        for rowno in range(len(board)):
+            for colno in range(len(board[0])):
+                
+                
+                val = board[rowno][colno] 
+                if val =='.':
                     continue
-                else:
-                    if  board[i][j] in r[i] or board[i][j] in c[j] or board[i][j] in b[(i//3,j//3)]:
-                        return False
-                    
-                    else: 
-                        r[i].add(board[i][j])
-                        c[j].add(board[i][j])
-                        b[(i//3,j//3)].add(board[i][j])
+                if val in row[rowno] or val in cols[colno] or val in box[(rowno//3,colno//3)]:
+                    return False
+                
+                row[rowno].add(val)
+                cols[colno] .add(val)
+                box[(rowno//3,colno//3)].add(val)
 
 
         return True
