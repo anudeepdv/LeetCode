b0252af92d3874c1698ba2cc858ea707067b1e3f
Anudeep D V <anudeepdv333@gmail.com>
2024-06-22 10:25:12 -0500

    Time: 35 ms (75.17%) | Memory: 16.7 MB (73.17%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,26 +1,22 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-
+        
         res=[]
-        s = []
+        s=[]
 
         def rec(l,r):
 
             if l==r==n:
                 res.append("".join(s))
-                return
 
-            if r<l:
-                s.append(')')
-                rec(l,r+1)
-                s.pop()
-            
             if l<n:
                 s.append('(')
                 rec(l+1,r)
                 s.pop()
+            if r<l:
+                s.append(')')
+                rec(l,r+1)
+                s.pop()
 
         rec(0,0)
-
         return res
-        
