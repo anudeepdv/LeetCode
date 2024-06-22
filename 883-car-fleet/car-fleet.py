068ae430f7f7714385a041f47ada460e0b2f32e3
Anudeep D V <anudeepdv333@gmail.com>
2024-06-22 11:00:44 -0500

    Time: 627 ms (97.87%) | Memory: 40.3 MB (11.54%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,20 +1,25 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+
+        res = [target - i for i in position]
+
+        z = list(zip(res,speed))
+
+        z.sort(key = lambda x:x[0])
+
+        r =[]
+
+        for i in z:
+            rem = i[0]/i[1]
+
+            if r:
+                if rem <= r[-1]:
+                    continue
+                else:
+                    r.append(rem) 
+            else:
+                r.append(rem)
+
+        return len(r)
+
         
-        res = [i for i in zip(position,speed)]
-       
-        res.sort(reverse=True,key= lambda x:x[0])
-
-        print(res)
-
-        q= collections.deque()
-        for i in res:
-
-            time = (target-i[0])/i[1]
-
-            q.append(time)
-
-            while len(q)>=2 and q[-1]<=q[-2]:
-                q.pop()
-
-        return len(q)
