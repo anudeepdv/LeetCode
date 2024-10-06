bb97325dc42b260a962fda21d63feb56c5038b1c
Anudeep D V <anudeepdv333@gmail.com>
2024-10-06 14:35:49 -0500

    Time: 7357 ms (24.63%) | Memory: 17.2 MB (91.39%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
@@ -1,29 +1,29 @@
-from collections import defaultdict
-
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        if not s or not words:
-            return []
         
-        word_count = defaultdict(int)
+        n = len(s)
+
+        wordlen = len(words[0])
+        d = defaultdict(int)
         for word in words:
-            word_count[word] += 1
-        
-        substr_len = len(words) * len(words[0])
-        word_len = len(words[0])
-        result = []
-        
-        for i in range(len(s) - substr_len + 1):
-            seen = defaultdict(int)
-            for j in range(i, i + substr_len, word_len):
-                word = s[j:j+word_len]
-                if word in word_count:
-                    seen[word] += 1
-                    if seen[word] > word_count[word]:
+            d[word] += 1
+        res= []
+        for i in range(n - (len(words)*wordlen)+1):
+            temp = collections.defaultdict(int)
+            # print(temp)
+            for j in range(i, i+wordlen*len(words),wordlen):
+                # print(s[j:j+wordlen])
+                word = s[j:j+wordlen]
+                if word in d:
+                    temp[word]+=1
+                    if temp[word] >d[word]:
                         break
                 else:
                     break
             else:
-                result.append(i)
-        
-        return result
+                res.append(i)
+                    
+            
+            
+
+        return res
