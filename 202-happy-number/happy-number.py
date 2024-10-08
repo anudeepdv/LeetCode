1617564c7622ecf9b07d93b2151f3d96feb0a52e
Anudeep D V <anudeepdv333@gmail.com>
2024-10-08 12:12:46 -0500

    Time: 42 ms (21.95%) | Memory: 16.6 MB (9.61%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -3,15 +3,19 @@ class Solution:
         
         v=set()
 
+        t = n
         while True:
 
-            t=0
-            for i in str(n):
-                t+=int(i)*int(i)
-            print(t)
-            if t==1:
+            cur=str(t)
+            print(cur)
+            res=0
+            for i in cur:
+                res+=int(i)*int(i)
+            
+            if res==1:
                 return True
-            if t in v:
+            elif res in v:
                 return False
-            v.add(t)
-            n=t
+
+            v.add(res)
+            t=res
