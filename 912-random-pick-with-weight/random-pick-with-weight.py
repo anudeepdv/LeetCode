1329f7d539ef99e53019671c3e4c368bbc4909ad
Anudeep D V <anudeepdv333@gmail.com>
2024-10-14 13:51:19 -0500

    Time: 138 ms (87.56%) | Memory: 21.2 MB (36.71%) - LeetSync

---
+++ 912-random-pick-with-weight/random-pick-with-weight.py
@@ -0,0 +1,26 @@
+class Solution:
+
+    def __init__(self, w: List[int]):
+        self.prefix=[]
+        total=0
+        for i in w:
+            total+=i
+            self.prefix.append(total)
+        self.total=total
+
+    def pickIndex(self) -> int:
+        
+        l= 0 
+        r=len(self.prefix)-1
+
+        rand =  random.uniform(0, self.total)
+        while l<r:
+
+            m = (l+r)//2
+
+            if rand>self.prefix[m]:
+                l=m+1
+            else:
+                r=m
+
+        return l
