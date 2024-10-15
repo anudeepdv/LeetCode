2cd0cf7055d36e136922b21462029abbb12dadec
Anudeep D V <anudeepdv333@gmail.com>
2024-10-14 20:11:20 -0500

    Time: 286 ms (5.09%) | Memory: 19.3 MB (72.63%) - LeetSync

---
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -0,0 +1,47 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        
+        i = 0 
+        cur=prev=res=0
+        cur_op = "+"
+
+        while i < len(s):
+
+            if s[i].isnumeric():
+
+                while i<len(s) and s[i].isnumeric():
+                    cur=cur*10
+                    cur+=int(s[i])
+                    i+=1
+
+                i-=1
+                print(cur,cur_op)
+                if cur_op =="+":
+                    res+=cur
+                    prev = cur
+                    print(res,"SS")
+                    
+                elif cur_op =="-":
+                    res-=cur
+                    prev=-cur
+                    
+                elif cur_op=="*":
+                    print(prev,cur,res)
+                    res=res-prev
+                    res+=prev*cur
+                    prev=prev*cur
+                elif cur_op=="/":
+                    print(prev,cur,res)
+                    res=res-prev
+                    res+=int(prev/cur)
+                    prev=int(prev/cur)
+
+                cur=0
+            elif s[i]!=' ':
+
+                cur_op=s[i]
+
+            i+=1
+
+        return res
+                
