fa49eb0c8b33842a35f68343793c535d130c5109
Anudeep D V <anudeepdv333@gmail.com>
2024-10-19 18:50:43 -0500

    Time: 0 ms (100.00%) | Memory: 16.7 MB (25.58%) - LeetSync

--- 670-maximum-swap/maximum-swap.py
+++ 670-maximum-swap/maximum-swap.py
@@ -1,42 +1,39 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
         
-        q = collections.deque()
+        q = deque()
 
-        while num:
+        cur = num
 
-            a = num%10
-            q.appendleft(a)
-            num=num//10
+        while cur!=0:
+            q.appendleft(cur%10)
+            cur=cur//10
 
-        arr = collections.deque()
+        
+        i = len(q)-1
+        maxseen = -1 
+        maxseeni=-1
 
-        max_seen, max_seen_i=-1,-1
-
-        i=len(q)-1
         while i>=0:
-            val = q[i]
-
-            if val > max_seen:
-                max_seen = val
-                max_seen_i = i
-
-            arr.appendleft([val,max_seen,max_seen_i])
+            cur = q[i]
+            if cur>maxseen:
+                maxseen = cur
+                maxseeni = i
+            q[i]=(cur,maxseen,maxseeni)
             i-=1
 
-        i = 0
-
-        while i<len(arr):
-            c,m,mi = arr[i]
-            if m>c:
-                print("J")
-                arr[i][0],arr[mi][0]=arr[mi][0],arr[i][0]
-                break
-            i+=1
-
-        res= 0
-        for i in arr:
-            res=res*10
-            res+=i[0]
+        print(q)
         
+        for i in range(len(q)):
+            if q[i][1]>q[i][0]:
+                q[i],q[q[i][2]]=q[q[i][2]],q[i]
+                break
+
+        
+
+
+        res=0 
+        for i in q:
+            res=res*10+i[0]
+
         return res
