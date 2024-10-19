08aba500cc2c9160f3e0f89f0d47d61ba891f7cc
Anudeep D V <anudeepdv333@gmail.com>
2024-10-19 12:47:39 -0500

    Time: 103 ms (63.44%) | Memory: 17.8 MB (100.00%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,47 +1,41 @@
 class Solution:
     def calculate(self, s: str) -> int:
         
-        i = 0 
         cur=prev=res=0
-        cur_op = "+"
 
-        while i < len(s):
+        cur_op='+'
 
+        i=0
+
+        while i<len(s):
             if s[i].isnumeric():
-
                 while i<len(s) and s[i].isnumeric():
-                    cur=cur*10
-                    cur+=int(s[i])
+                    cur=cur*10+int(s[i])
                     i+=1
+                i=i-1
 
-                i-=1
-                # print(cur,cur_op)
                 if cur_op =="+":
-                    res+=cur
-                    prev = cur
-                    # print(res,"SS")
-                    
-                elif cur_op =="-":
-                    res-=cur
+                    res=res+cur
+                    prev=cur
+
+                elif cur_op=='-':
+                    res=res-cur
                     prev=-cur
-                    
-                elif cur_op=="*":
-                    # print(prev,cur,res)
+                elif cur_op =="*":
                     res=res-prev
                     res+=prev*cur
-                    prev=prev*cur
-                elif cur_op=="/":
-                    # print(prev,cur,res)
+                    prev = prev*cur
+                elif cur_op =="/":
                     res=res-prev
                     res+=int(prev/cur)
-                    prev=int(prev/cur)
+                    prev = int(prev/cur)
 
                 cur=0
-            elif s[i]!=' ':
 
+            elif s[i]!=' ':
                 cur_op=s[i]
 
             i+=1
 
         return res
-                
+
