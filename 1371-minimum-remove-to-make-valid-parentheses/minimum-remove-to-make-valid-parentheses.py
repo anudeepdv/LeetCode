25648631126ae3ae7957b82746054b038a1909aa
Anudeep D V <anudeepdv333@gmail.com>
2024-10-19 12:03:18 -0500

    Time: 796 ms (5.06%) | Memory: 17.9 MB (88.79%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -2,24 +2,19 @@ class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         stack=collections.deque()
         r=[]
-        for  index,i in enumerate(s):
-            if i ==')' and stack:
-                stack.pop()
-            elif i==')':
-                r.append(index)
-            elif i=='(':
-                stack.append(index)
-
-        # print(r,stack)
-
-        res=r+list(stack)
-        print(res)
-
-        output=""
-        for i,val in enumerate(s):
-            if i not in res:
-                output+=val
-        return output
-
-        return ""
         
+        for i,val in enumerate(s):
+            if val ==')' and stack:
+                stack.pop()
+            elif val== ')':
+                r.append(i)
+            elif val=='(':
+                stack.append(i)
+
+        res =''
+
+        for i,val in enumerate(s):
+            if i not in stack and i not in r:
+                res+=val
+
+        return res
