7edf177c3489eabeddb830bbeab6f93626b43233
Anudeep D V <anudeepdv333@gmail.com>
2024-10-20 13:45:28 -0500

    Time: 103 ms (78.19%) | Memory: 17.2 MB (84.47%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,34 +1,36 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         
-        l=0
-        
-        window = collections.defaultdict(int)
-        tmap = Counter(t)
-        need = len(tmap)
+        l = 0
+        nmap = Counter(t)
+        need = len(nmap)
 
-        have = 0
-        besl,besr=0,0
-        best = math.inf
-        for r in range(len(s)):
-            if s[r] in tmap :
-                
-                window[s[r]]+=1
-                if tmap[s[r]]==window[s[r]]:
+        hmap = collections.defaultdict(int)
+        have =0
+
+        res=math.inf
+        besl , besr = 0,0
+        for  r in range(len(s)):
+            if s[r] in nmap:
+                hmap[s[r]]+=1
+                if hmap[s[r]]==nmap[s[r]]:
                     have+=1
-                # print(window,have,tmap)
             
-            while have == need:
-                if r-l+1<best:
-                    best = r-l+1
-                    besl,besr=l,r
-
-                if s[l] in tmap:
-                    window[s[l]]-=1
-                    if window[s[l]]<tmap[s[l]]:
+            while have==need:
+                # print(hmap,nmap)
+                if r-l+1<res:
+                    besl = l
+                    besr=r
+                    res=r-l+1
+                    print(res,"res")
+                if s[l] in nmap:
+                    hmap[s[l]]-=1
+                    if hmap[s[l]]<nmap[s[l]]:
                         have-=1
+                        # print("Have",have)
 
-                l=l+1
-        # print(best)
-        return s[besl:besr+1] if best!=math.inf else ""
+                l+=1
 
+        return s[besl:besr+1] if res!=math.inf else ""
+                    
+                
