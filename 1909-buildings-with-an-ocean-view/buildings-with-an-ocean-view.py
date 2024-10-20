bc72ad66317ec11ea5c724dce6473655e3fdeb9b
Anudeep D V <anudeepdv333@gmail.com>
2024-10-19 19:24:20 -0500

    Time: 11 ms (99.71%) | Memory: 30.4 MB (98.51%) - LeetSync

--- 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
@@ -1,11 +1,9 @@
 class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
-        
-        res= []
-        maxh = 0
-
-        for  i in range(len(heights)-1,-1,-1):
-            if heights[i]>maxh:
-                res.append(i)
-                maxh=heights[i]
-        return res[::-1]
+        curHtg=-1
+        res=collections.deque([])
+        for i in range(len(heights)-1,-1,-1):
+            if curHtg<heights[i]:
+                curHtg=heights[i]
+                res.appendleft(i)
+        return list(res)
