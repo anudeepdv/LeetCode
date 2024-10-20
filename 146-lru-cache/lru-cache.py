f336ea04c62414830cbd12da455fcdff4a7279a2
Anudeep D V <anudeepdv333@gmail.com>
2024-10-20 12:43:57 -0500

    Time: 139 ms (99.28%) | Memory: 77.2 MB (95.61%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,85 +1,72 @@
 class Node:
-    def __init__(self,key,val) -> None:
-        self.key=key
-        self.val=val
-        self.next=None
+
+    def __init__(self,key,val):
+        self.key = key
+        self.val =val
         self.prev=None
+        self.next=None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cap=capacity
-        self.left=Node(0,0)
-        self.right=Node(0,0)
-
-        self.left.next=self.right
+        self.left = Node(-1,-1)
+        self.right = Node(-1,-1)
+        self.left.next = self.right
         self.right.prev=self.left
-        self.map={}
+        self.m={}
+        self.capacity=capacity
+
+    def addbegining(self, node: Node) -> None :
+        temp = self.left.next
+        self.left.next = node
+        node.prev= self.left
+        node.next=temp
+        temp.prev=node
+
+    def remove(self, node: Node) -> None :
+
+        nodel = node.prev
+        noder=node.next
+        nodel.next = noder
+        noder.prev=nodel
 
 
     def get(self, key: int) -> int:
 
+        if key in self.m:
 
-        if key not in self.map:
+            node = self.m[key]
+            self.remove(node)
+            self.addbegining(node)
+
+            return node.val
+
+        else:
             return -1
-        
-        node=self.map[key]
-        nodel=node.prev
-        noder=node.next
-        nodel.next=noder
-        noder.prev=nodel
-
-        nodel=self.right.prev
-        nodel.next=node
-        node.prev=nodel
-        node.next=self.right
-        self.right.prev=node
-
-        return node.val
-
         
 
     def put(self, key: int, value: int) -> None:
-        
-        if key in self.map:
-            node = self.map[key]
-            nodel=node.prev
-            noder=node.next
-            nodel.next=noder
-            noder.prev=nodel
-            del self.map[key]
-            
-        elif len(self.map)==self.cap:     
+        # print(key,value)
+        if key in self.m:
+            node = self.m[key]
+            node.val = value
+            self.remove(node)
+            self.addbegining(node)
 
-            node=self.left.next
-            self.left.next=node.next
-            node.next.prev=self.left
-            keyr=node.key
-            del self.map[keyr]
-      
+        else:
+            if len(self.m)==self.capacity:
+                temp  = self.right.prev
+                del self.m[temp.key]
+                self.remove(temp)
+                node=Node(key,value)
+                self.addbegining(node)
+                self.m[key]=node         
+            else:
+                node=Node(key,value)
+                self.addbegining(node)
+                self.m[node.key]=node
 
-
-
-        node = Node(key,value)
-        
-        node.next=self.right
-        node.prev=self.right.prev
-        self.right.prev.next=node
-        self.right.prev=node
-        self.map[key]=node
-        l=self.left
-        
-
-
-        
-        
-        
-
-
-
-
-        
-
+        # print(self.m.keys())
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
