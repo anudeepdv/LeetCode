d1232a8e5237dbe6b9b0f84c8160d599dc8469c1
Anudeep D V <anudeepdv333@gmail.com>
2024-10-22 15:29:07 -0500

    Time: 15 ms (94.53%) | Memory: 17 MB (5.70%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -1,27 +1,30 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
         
-        l = 0
-        r= 1
-        while l< len(s) and s[l]==" ":
-            l=l+1
-        r=l+1
-        res= []
-        while r<len(s):
-            # print(s[l:r])
+        q = collections.deque()
 
+        l= 0
+
+        
+
+        r=l
+
+        for r in range(len(s)):
             if s[r]==" ":
-                if s[l:r]!="" and s[l:r]!=" " :
-                    res.append(s[l:r])
-                while  r<len(s) and s[r]==" ":
+                print(l,r)
+                if s[l:r+1]==" ":
                     l=r+1
-                    r=r+1
-            r=r+1
-        print(res)
-        if s[l:r]!="" and s[l:r]!=" " :
-            res.append(s[l:r])
-        
-        l= res[::-1]
+                else:
+                    q.appendleft(s[l:r])
+                    l=r+1
 
-        
-        return " ".join(l)
+        if s[l:r+1]!='':
+
+            q.appendleft(s[l:r+1])
+
+        return ' '.join(q)
+            
+
+        print(q)
+
+                
