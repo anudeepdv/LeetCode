470f51ac1de3782a70d05f4e4226a86bae4abbcc
Anudeep D V <anudeepdv333@gmail.com>
2024-10-22 13:40:53 -0500

    Time: 3 ms (97.24%) | Memory: 17 MB (14.27%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,17 +1,16 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         
-
         res=[]
 
-        def dfs(comb,cur):
-            print(comb)
-            if len(comb)==len(nums):
-                res.append(comb.copy())
+        def dfs(i,l,l1):
+            print(l,l1)
+            if i == len(nums):
+                res.append([k for k in l])
                 return
 
-            for i in range(len(cur)):
-                dfs(comb+[cur[i]], cur[:i]+cur[i+1:])
+            for j in range(len(l1)):
+                dfs(i+1,l+[l1[j]],l1[:j]+l1[j+1:])
 
-        dfs([],nums)
+        dfs(0,[],nums)
         return res
