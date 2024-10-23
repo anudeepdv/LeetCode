b1b19d6c8b0393c5bb35c7abac988e512999813f
Anudeep D V <anudeepdv333@gmail.com>
2024-10-23 15:25:45 -0500

    Time: 0 ms (100.00%) | Memory: 16.7 MB (33.50%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -4,13 +4,12 @@ class Solution:
         r= len(nums)-1
 
         while l<=r:
+
             m = (l+r)//2
-
-            #left gre
-
             if m>0 and nums[m-1]>nums[m]:
                 r=m-1
-            elif m<len(nums)-1 and nums[m+1]>nums[m]:
+            elif m<len(nums)-1 and nums[m]<nums[m+1]:
                 l=m+1
             else:
                 return m
+
