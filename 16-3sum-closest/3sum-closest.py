8b3ad14cca0304ad50260a4202dbbc2126039c94
Anudeep D V <anudeepdv333@gmail.com>
2024-10-23 23:36:24 -0500

    Time: 374 ms (74.94%) | Memory: 16.7 MB (17.70%) - LeetSync

--- 16-3sum-closest/3sum-closest.py
+++ 16-3sum-closest/3sum-closest.py
@@ -4,27 +4,25 @@ class Solution:
         nums.sort()
         res=math.inf
         
+        for i in range(len(nums)):
 
-        for  i in range(len(nums)):
-
-            l = i+1
-            r=len(nums)-1
-
-            if i>0 and nums[i-1]==nums[i]:
+            if i>0 and nums[i]==nums[i-1]:
                 continue
 
-            while l<r:
-                # print(i,l,r)
-                tot = nums[i]+nums[l]+nums[r]
+            l= i+1 
+            r=len(nums)-1
 
-                if tot == target:
+            while l<r:
+                tot = nums[i]+nums[l]+nums[r]
+                if tot==target:
                     return target
-                
-                if abs(tot-target) < abs(res-target):
+                if abs(tot-target)<abs(res-target):
                     res=tot
+
                 if tot>target:
                     r-=1
                 else:
-                    l+=1
+                    l=l+1
 
         return res
+                
