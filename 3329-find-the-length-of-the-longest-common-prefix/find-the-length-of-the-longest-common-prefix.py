2f22a845538a23638036dd8bc2639ac28a465bd3
Anudeep D V <anudeepdv333@gmail.com>
2024-10-23 23:25:40 -0500

    Time: 130 ms (99.98%) | Memory: 28 MB (95.44%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.py
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.py
@@ -1,20 +1,22 @@
 class Solution:
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
-        if len(arr2)>len(arr1):
+        
+        if len(arr2) > len(arr1):
             arr1,arr2 = arr2,arr1
-        hm = set()
+
+        vis = set()
 
         for n in arr1:
 
-            while n and n not in hm:
-                hm.add(n)
+            while n and n not in vis:
+                vis.add(n)
                 n=n//10
-        
+
         res=0
         for n in arr2:
-            while n and n not in hm:
+            while n and n not in vis:
                 n=n//10
-            
+
             if n:
                 res=max(res,len(str(n)))
 
