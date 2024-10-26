af6a74f36a9c3a9619ccce16d308fbdebfb88921
Anudeep D V <anudeepdv333@gmail.com>
2024-10-25 20:39:24 -0500

    Time: 19 ms (98.43%) | Memory: 16.5 MB (94.00%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,10 +1,33 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
 
-        a1 = sorted(s1)
+        # print(ord("a"),ord("z"),ord("z")-ord("a"))
+        s1_list =[0]*26
 
-        for i in range(0,len(s2)-len(s1)+1):
-            if a1==sorted(s2[i:i+len(a1)]):
-                return True
+        for i in s1:
+            s1_list[ord(i)-ord('a')]+=1
+        # print(s1_list)
+        k=len(s1)
+        l=0
+        s2_list=[0]*26
+        for r in range(len(s2)):
+            if r<k:
+                # print(s2[r])
+                s2_list[ord(s2[r])-ord('a')]+=1
+                if r-l+1==k:
+                    # print(s2[l:r+1])
+                    if tuple(s2_list)==tuple(s1_list):
+                        return True
+
+            else:
+                s2_list[ord(s2[r])-ord('a')]+=1
+                s2_list[ord(s2[l])-ord('a')]-=1
+                l+=1   
+                if tuple(s2_list)==tuple(s1_list):
+                        return True       
+                # print(s2[l:r+1])
+
 
         return False
+
+
