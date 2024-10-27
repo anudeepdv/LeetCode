6227e73b2f693ea8a5b62a9b95b82deacad5e1ed
Anudeep D V <anudeepdv333@gmail.com>
2024-10-26 20:39:49 -0500

    Time: 11 ms (75.12%) | Memory: 18.3 MB (84.69%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,27 +1,28 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-       
-        l=[0]
-        curleft =height[0]
-
-        for i in range(1,len(height)):
-            l.append(curleft)
-            curleft=max(curleft,height[i])
-
-        print(l)
-
-        r=[0]
-        curright = height[-1]
-        for i in range(len(height)-2,-1,-1):
-            r.insert(0,curright)
-            curright=max(curright,height[i])
-
-        print(r)
-
+        
         res=0
 
-        for i in range(len(height)):
-            val = min(l[i],r[i])-height[i]
-            if val>0:
-                res=res+val
+        maxl,maxr = height[0],height[-1]
+
+        l=0
+        r=len(height)-1
+
+        while l<r:
+
+            if maxl<maxr:
+                l+=1
+                trap = maxl - height[l]
+                if trap>0:
+                    res+=trap
+                maxl = max(maxl,height[l])
+                
+            else:
+                r-=1
+                trap = maxr -height[r]
+                if trap>0:
+                    res+=trap
+                maxr = max(maxr,height[r])
+                
+
         return res
