2ac87e9f3043d6dd64a01805ac07abfc8bb2ef94
Anudeep D V <anudeepdv333@gmail.com>
2024-10-28 19:50:43 -0500

    Time: 0 ms (100.00%) | Memory: 16.7 MB (20.24%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -4,26 +4,26 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
-        index =-1
+        index = -1
 
+        #find last peak
         for i in range(1,len(nums)):
-
             if nums[i]>nums[i-1]:
-                index =i
+                index=i
 
         if index ==-1:
-            print("k")
             nums.reverse()
-            return        
+            return
+        
         peak = nums[index]
         peaki = index
         prev = nums[index-1]
-        print(prev,"PPREC")
-        print(peak,peaki,"PEK")
-        for i in range(peaki+1,len(nums)):
+
+        for  i in range(peaki+1,len(nums)):
             if nums[i]>prev and nums[i]<=peak:
-                peaki = i 
                 peak = nums[i]
-                print(peak, "peak")
-        nums[peaki] ,nums[index-1] = nums[index-1], nums[peaki]
+                peaki = i
+
+        nums[peaki],nums[index-1] = nums[index-1],nums[peaki]
         nums[index:]=reversed(nums[index:])
+
