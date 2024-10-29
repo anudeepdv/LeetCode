9e9c20d8ea8c3a905d1cffea929be99d0b594380
Anudeep D V <anudeepdv333@gmail.com>
2024-10-29 14:50:25 -0500

    Time: 0 ms (100.00%) | Memory: 16.6 MB (74.56%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,31 +1,33 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        
-        res=''
-        for i in range(len(s)):
-            val = s[i]
-
-            if val==' ' and res=='':
-                continue
-            elif res=='' and val in ['+','-']:
-               
-                res+=val+'0'
-
-            elif val in "0123456789":
-                res+=val
-            else:
-                break
-        print(res)
-        if res=='':
+        if not s:
             return 0
-        
+        res=0
+        l=0
+        sign=1
+        while l <len(s) and s[l]==" ":
+            l+=1
+        if l <len(s) and s[l] in "+-":
+            sign=1 if s[l]=='+' else -1
+            l+=1
 
-        if int(res)< -2**31:
+        while l <len(s) and s[l]=='0':
+            l+=1
+        
+        while l<len(s):
+
+            if s[l] not in "0123456789":
+                break
+            else:
+                res=res*10+int(s[l])
+            l+=1
+
+        
+        resn = 0 if res in('','-','+') else sign*res
+
+        if resn< -2**31:
             return -2**31
-        elif int(res)>2**31-1:
+        elif resn> 2**31-1:
             return 2**31-1
 
-        return int(res)
-
-
-            
+        return resn
