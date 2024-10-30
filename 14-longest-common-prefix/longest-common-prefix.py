64fc298d013ba19c24b1c3df5e823db448cbe7ff
Anudeep D V <anudeepdv333@gmail.com>
2024-10-29 20:52:27 -0500

    Time: 0 ms (100.00%) | Memory: 16.6 MB (46.56%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,16 +1,52 @@
+class TrieNodee:
+    def __init__(self):
+        self.children = {}
+        self.eod = False
+
+class Trie:
+    def __init__(self):
+        self.root=TrieNodee()
+
+    def addword(self,word):
+        cur = self.root
+
+        for w in word:
+            
+            if w not in cur.children:
+                cur.children[w] = TrieNodee()
+            cur =cur.children[w]
+
+        cur.eod=True
+    
+    def checkprefix(self):
+        cur =self.root
+
+        res=""
+       
+        while cur :
+            print(cur.children.keys())
+            if len(cur.children)==1:
+                res+=list(cur.children.keys())[0]
+                cur=cur.children[list(cur.children.keys())[0]]
+            else:
+                break
+            if cur.eod==True:
+                break
+            
+        
+        return res
+                
+
+    
+
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         
-        first = strs[0]
+        trie = Trie()
 
-        res=''
-        
-        for i in range(len(first)):
-            for j in range(1,len(strs)):
-                word = strs[j]
-                if i ==len(word) or word[i]!=first[i]:
-                    return res
+        for w in strs:
+            if not w:
+                return ""
+            trie.addword(w)
 
-            res+=first[i]
-
-        return res
+        return trie.checkprefix()
