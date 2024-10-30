ebf5960bd29d6711472e301b8f2b799e0563e305
Anudeep D V <anudeepdv333@gmail.com>
2024-10-29 21:00:40 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (27.97%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -4,21 +4,22 @@ class Solution:
 
         def binary(l,r,left):
             res=-1
+
             while l<=r:
                 m = (l+r)//2
 
-                if target>nums[m]:
-                    l=m+1
-                elif target<nums[m]:
-                    r=m-1
+                if nums[m]>target:
+                    r=m-1  
+                elif nums[m]<target:
+                    l=m+1 
+
                 else:
                     res=m
-                    if left:
+                    if left:  
                         r=m-1
                     else:
                         l=m+1
 
-            return res
-        
+            return res        
 
         return [binary(0, len(nums)-1, True),binary(0, len(nums)-1, False)]
