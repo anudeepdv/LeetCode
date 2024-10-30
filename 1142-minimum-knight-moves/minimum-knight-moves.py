51cbb0aed37f4697804f5bd46fd3092c1d3363e8
Anudeep D V <anudeepdv333@gmail.com>
2024-10-30 02:36:19 -0500

    Time: 3294 ms (50.00%) | Memory: 78.6 MB (25.32%) - LeetSync

--- 1142-minimum-knight-moves/minimum-knight-moves.py
+++ 1142-minimum-knight-moves/minimum-knight-moves.py
@@ -5,29 +5,22 @@ class Solution:
         dir = [(1, 2), (2, 1), (2, -1), (1, -2),
                    (-1, -2), (-2, -1), (-2, 1), (-1, 2)]
 
-        q=collections.deque()
-
-        q.append((0,0))
-
+        q = deque()
+        q.append((0,0,0))
         vis=set()
         vis.add((0,0))
-        res=0
+
         while q:
-            for _ in range(len(q)):
-                
-                x1,y1 = q.popleft()
 
-                if x1==x and y==y1:
-                    return res
+            i,j,r = q.popleft()
 
-                for dx,dy in dir:
-                    nx,ny = x1+dx,y1+dy
+            if i==x and j==y:
+                return r
+            
+            for ix,ij in dir:
+                nx,ny = i+ix,j+ij
+                if (nx,ny) not in vis:
+                    q.append((nx,ny,r+1))
+                    vis.add((nx,ny))
 
-                    if (nx,ny) not in vis:
-                        q.append((nx,ny))
-                        vis.add((nx,ny))
-
-            res+=1
-
-        
-
+            
