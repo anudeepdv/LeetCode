1eb10d9adfe2c594d55ed2e2806aa8e4703e83e6
Anudeep D V <anudeepdv333@gmail.com>
2024-10-30 20:06:48 -0500

    Time: 1968 ms (5.01%) | Memory: 30.9 MB (40.85%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.py
+++ 854-making-a-large-island/making-a-large-island.py
@@ -7,58 +7,54 @@ class Solution:
         island_id=-1
         dir=[(1,0),(0,1),(-1,0),(0,-1)]
         area_map = collections.defaultdict(int)
-        def dfs(x,y,vis):
-
-
+        
+        def dfs(x,y):
             if x not in range(rows) or y not in range(cols):
-                return
-
-            if (x,y) in vis:
                 return
 
             if grid[x][y]!=1:
                 return
+
             
-            vis.add((x,y))
-            grid[x][y]=island_id
+            grid[x][y] = island_id
             area_map[island_id]+=1
-
-            for nx,ny in dir:
-                dfs(x+nx,y+ny,vis)
-
+            for i, j in dir:
+                nx,ny = x+i,y+j
+                dfs(nx,ny)
 
 
         for r in range(rows):
             for c in range(cols):
-
                 if grid[r][c]==1:
-                    dfs(r,c,set())
+                    dfs(r,c)
                     island_id-=1
+
+        print(grid)
+
         max_area=0
         for r in range(rows):
             for c in range(cols):
                 
                 if grid[r][c]==0:
-                    
                     area=1
-                    sur_set = set()
+                    sur_set=set()
 
-                    for i,j in dir:
-                        nx ,ny = r+i,c+j
-                        if nx in range(rows) and ny in range(cols) and grid[nx][ny]!=0:
+                    for x,y in dir:
+                        nx,ny = r+x,c+y
+                        if nx in range(rows) and ny in range(cols):
                             sur_set.add(grid[nx][ny])
+                    for id in sur_set:
+                        area+=area_map[id]
 
-                    for sur in sur_set:
-                        area+=area_map[sur]
+                    max_area = max(max_area,area)
 
-                    max_area=max(max_area,area)
 
         return max_area if max_area!=0 else rows*cols
 
-                    
-                        
 
 
+
+       
 
         
 
