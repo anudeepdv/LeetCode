c460f70f71386d54b1cd820c7ae2385a1cbb1572
Anudeep D V <anudeepdv333@gmail.com>
2024-10-30 19:45:37 -0500

    Time: 91 ms (74.27%) | Memory: 32 MB (18.85%) - LeetSync

--- 480-sliding-window-median/sliding-window-median.py
+++ 480-sliding-window-median/sliding-window-median.py
@@ -1,40 +1,47 @@
 class Solution:
     def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
         
-        maxh = []
         minh = []
+        maxh = []
 
         for i in range(k):
             heappush(maxh, -nums[i])
-            heappush(minh, -heappop(maxh))
-
+            heappush(minh,-heappop(maxh))
             if len(minh)>len(maxh):
-                heappush(maxh, -heappop(minh))
-        
-        median =None
+                heappush(maxh,-heappop(minh))
+
+        median = None
         res=[]
-        if k%2==1:
+        if k%2:
             median = -maxh[0]
         else:
-            median = (-maxh[0]+minh[0])/2
+            median = (-maxh[0] +minh[0])/2
         res.append(median)
+        print(median)
+
         hmap = collections.defaultdict(int)
+
         for i in range(k,len(nums)):
+            
             prev = nums[i-k]
+            balance=0
             hmap[prev]+=1
-            balance = -1 if prev<=median else 1
+            if prev<=median:
+                balance-=1
+            else:
+                balance+=1
 
             if nums[i]<=median:
-                balance+=1
                 heappush(maxh, -nums[i])
+                balance+=1
             else:
-                balance-=1
                 heappush(minh, nums[i])
-            
-            if balance<0:
-                heappush(maxh, -heappop(minh))
-            elif balance>0:
+                balance-=1
+
+            if balance>0:
                 heappush(minh, -heappop(maxh))
+            elif balance<0:
+                heappush(maxh, -heappop(minh))
 
             while maxh and hmap[-maxh[0]]:
                 hmap[-maxh[0]]-=1
@@ -43,12 +50,14 @@ class Solution:
             while minh and hmap[minh[0]]:
                 hmap[minh[0]]-=1
                 heappop(minh)
-                
 
-            if k%2==1:
+            if k%2:
                 median = -maxh[0]
             else:
-                median = (-maxh[0]+minh[0])/2
+                median = (-maxh[0] +minh[0])/2
             res.append(median)
 
         return res
+
+
+            
