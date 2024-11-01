5b1bf1254f4572aa9761d9456268b7477f727c70
Anudeep D V <anudeepdv333@gmail.com>
2024-10-31 19:44:18 -0500

    Time: 0 ms (100.00%) | Memory: 16.5 MB (62.48%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -11,26 +11,25 @@ class Solution:
         r=n
 
         while len(res)<m*n:
-            print(res)
+
             for i in range(l,r):
                 res.append(matrix[t][i])
             t+=1
 
             for i in range(t,b):
                 res.append(matrix[i][r-1])
-            r=r-1
+            r-=1
 
-            print(l,r,t,b)
-            if  not (l<r and t<b):
+            if t>=b or l>=r:
                 break
+
             for i in range(r-1,l-1,-1):
-                print(b,i)
                 res.append(matrix[b-1][i])
             b-=1
 
             for i in range(b-1,t-1,-1):
                 res.append(matrix[i][l])
+
             l+=1
 
         return res
-        
