bc675a699808a4a9ca2a0ae12dde4cb4bf031c84
Anudeep D V <anudeepdv333@gmail.com>
2024-11-02 12:22:20 -0500

    Time: 125 ms (12.44%) | Memory: 20.7 MB (30.33%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,53 +1,43 @@
 class Solution:
     def calculate(self, s: str) -> int:
         
-        q = []
-
         i = 0
+
+        q= []
 
         cur = 0
 
-        sign = '+'
+        sign = "+"
 
-        while i<len(s):
+        while i < len(s):
+
             if s[i].isdigit():
-                while i<len(s) and s[i].isdigit():
-                    cur=cur*10+int(s[i])
+                while i < len(s) and s[i].isdigit():
+                    cur = cur * 10 + int(s[i])
                     i+=1
                 i-=1
+
             if s[i] in "+-/*" or i==len(s)-1:
-                if sign == '+':
+                if sign=="+":
                     q.append(cur)
-                    cur=0
-                    sign=1
-
-                elif sign=='-':
+                elif sign=="-":
                     q.append(-cur)
-                    cur=0
-                    sign=-1
-
-                elif sign=='*':
-                    res=cur*q.pop() 
-                    q.append(res)
-                    cur=0
-
-                elif sign=='/':
-                    res=int(q.pop()/cur)
-                    q.append(res)
-                    cur=0
+                elif sign=="*":
+                    a=q.pop()
+                    q.append(cur*a)
+                elif sign=="/":
+                    a=q.pop()
+                    q.append(int(a/cur))
 
                 sign=s[i]
+                cur=0
             i+=1
-
-    
-
-        # print(q)
+            
+        print(q)
         res=0
-
         for i in q:
             res+=i
 
         return res
-
 
 
