e5a2e1a23e54a40063e447164984cb25336c89ea
Anudeep D V <anudeepdv333@gmail.com>
2024-11-02 00:33:16 -0500

    Time: 126 ms (91.50%) | Memory: 70 MB (79.37%) - LeetSync

--- 2019-product-of-two-run-length-encoded-arrays/product-of-two-run-length-encoded-arrays.py
+++ 2019-product-of-two-run-length-encoded-arrays/product-of-two-run-length-encoded-arrays.py
@@ -5,34 +5,33 @@ class Solution:
         i=0
         j=0
 
-        prev=None
-        while i <len(encoded1) and j<len(encoded2):
-            val = None
+        while i<len(encoded1) and j<len(encoded2):
+            val =None
+            count=None
             if encoded1[i][1]==encoded2[j][1]:
-                val=[encoded1[i][0]*encoded2[j][0],encoded1[i][1]]
+                val = encoded1[i][0]*encoded2[j][0]
+                count = encoded1[i][1]
                 i+=1
                 j+=1
-            elif encoded1[i][1] >encoded2[j][1]:
-                val=[encoded1[i][0]*encoded2[j][0],encoded2[j][1]]
-                encoded1[i][1]-=encoded2[j][1]
-                j+=1
-            else:
-                val=[encoded1[i][0]*encoded2[j][0],encoded1[i][1]]
-                encoded2[j][1]-=encoded1[i][1]
+            elif encoded1[i][1]<encoded2[j][1]:
+                val = encoded1[i][0]*encoded2[j][0]
+                count = encoded1[i][1]
+                encoded2[j][1]-=count
                 i+=1
-
-            if prev:
-                if val[0]==prev[0]:
-                    prev = [prev[0],prev[1]+val[1]]
-                else:
-                    res.append(prev)
-                    prev=val
             else:
-                prev=val
+                val = encoded1[i][0]*encoded2[j][0]
+                count = encoded2[j][1]
+                encoded1[i][1]-=count
+                j+=1
 
-        if prev:
-            res.append(prev)
+            if res and res[-1][0]==val:
+                res[-1][1]+=count
+            else:
+                res.append([val, count])
+
+            # print(res)
+
         return res
 
-
-
+            
+                
