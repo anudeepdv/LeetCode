6e55ac2d3ec7d677ee53fc07e77904a74027fae3
Anudeep D V <anudeepdv333@gmail.com>
2024-11-03 19:36:26 -0600

    Time: 28 ms (62.55%) | Memory: 29.6 MB (73.57%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -1,42 +1,13 @@
-class TrieNode:
-    def __init__(self):
-        self.children={}
-        self.eof=False
-
-class Trie:
-
-    def __init__(self):
-        self.root = TrieNode()
-        self.res=[]
-        
-    def insert(self, fld: str) -> None:
-
-        folder = fld.split('/')
-        cur = self.root
-        flag=True
-        for w in folder:
-           
-            if w!="":
-                if cur.eof:
-                    flag=False
-                if w not in cur.children:
-                    cur.children[w]=TrieNode()
-
-                cur=cur.children[w]
-        cur.eof=True
-        if flag:
-            self.res.append(fld)
-
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
-        folder.sort()
-        T = Trie()
-
-        for f in folder:
-            T.insert(f)
-
-        return T.res
-
         
-            
+        folder.sort()
+        seen = [folder[0]]
 
+        for i in folder[1:]:
+            if i.startswith(seen[-1]+'/'):
+                continue
+            else:
+                seen.append(i)
+
+        return seen
