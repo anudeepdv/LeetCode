1044b303fc1f443460fc7e81478cdd922f3cf87f
Anudeep D V <anudeepdv333@gmail.com>
2024-11-03 21:31:22 -0600

    Time: 36 ms (86.64%) | Memory: 44.1 MB (39.39%) - LeetSync

--- 1731-even-odd-tree/even-odd-tree.py
+++ 1731-even-odd-tree/even-odd-tree.py
@@ -1,49 +1,45 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
-        
-        lvl = collections.defaultdict(list)
+        # Create a queue for nodes that need to be visited and add the root
+        queue = deque()
+        current = root
+        queue.append(current)
 
-        res= True
-        def dfs(node,i):
-            nonlocal res
+        # Keeps track of whether we are on an even level
+        even = True
 
-            if not node:
-                return
-            
-            if i%2==0:
-                if node.val%2 ==0:
-                    res=False
+        # While there are more nodes in the queue
+        # Determine the size of the level and handle the nodes
+        while queue:
+            size = len(queue)
+
+            # Prev holds the value of the previous node in this level
+            prev = float("inf")
+            if even:
+                prev = -prev
+
+            # While there are more nodes in this level
+            # Remove a node, check whether it satisfies the conditions
+            # Add its children to the queue
+            while size > 0:
+                current = queue.popleft()
+
+                # If on an even level, check that the node's value is odd and greater than prev
+                # If on an odd level, check that the node's value is even and less than prev
+                if (even and (current.val % 2 == 0 or current.val <= prev)) or \
+                        (not even and (current.val % 2 == 1 or current.val >= prev)):
                     return False
-                if i in lvl:
-                    if node.val<=lvl[i][-1]:
-                        res=False
-                        return False
-                    else:
-                        lvl[i].append(node.val)
-                else:
-                    lvl[i].append(node.val)
-            else:
-                if node.val%2 ==1:
-                    res=False
-                    return False
-                if i in lvl:
-                    if  node.val>=lvl[i][-1]:
-                        res=False
-                        return False
-                    else:
-                        lvl[i].append(node.val)
-                else:
-                    lvl[i].append(node.val)
 
-            dfs(node.left,i+1)
-            dfs(node.right,i+1)
+                prev = current.val
+                if current.left:
+                    queue.append(current.left)
+                if current.right:
+                    queue.append(current.right)
+                # Decrement size, we have handled a node on this level
+                size -= 1
 
-        dfs(root,0)
-        print(lvl)
-        return res
+            # Flip the value of even, the next level will be opposite
+            even = not even
+
+        # If every node meets the conditions, the tree is Even-Odd
+        return True
