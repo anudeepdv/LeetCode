5d2233318d2dfff6a6b83d244980ebb23d1f2c95
Anudeep D V <anudeepdv333@gmail.com>
2026-01-01 21:16:11 -0600

    Time: 11 ms (36.55%) | Memory: 17.5 MB (95.98%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -6,15 +6,15 @@ class Solution:
         i =0 
         j = len(s)-1
 
-        while i<=j:
+        while i<j:
 
-            while i<=j and not s[i].isalnum():
+            while i<j and not s[i].isalnum():
                 i+=1
-            while i<=j and not s[j].isalnum():
+            while i<j and not s[j].isalnum():
                 j-=1
-            if i<=j and s[i].lower()!=s[j].lower():
+            if s[i].lower()!=s[j].lower():
                 return False
-            if i<=j:
+            if i<j:
                 i+=1
                 j-=1
         return True
