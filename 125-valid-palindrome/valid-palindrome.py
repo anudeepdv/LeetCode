8afabb2f42ff5d5dae9b7485a6c4514ef39ca648
Anudeep D V <anudeepdv333@gmail.com>
2026-01-01 21:12:32 -0600

    Time: 34 ms (6.29%) | Memory: 17.9 MB (83.49%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -8,22 +8,15 @@ class Solution:
         print(ord("9"))
 
        
-        a=''
-        for i in s:
-            if ord(i) in range(97,123) or ord(i) in range(65,91) or ord(i) in range(48,58):
-                a=a+i.lower()
-        print(a)
-        
-        l =0 
-        r= len(a)-1
-
-       
+        res = ''
+        for v in s:
+            if ord(v) in range(97,123) or  ord(v) in range(65,91) or  ord(v) in range(48,58):
+                res=res+v.lower()
+        l,r=0,len(res)-1
+        print(res)
         while l<=r:
-
-            if a[l]!=a[r]:
-                return False
-
-            l=l+1
-            r=r-1
-
-        return True
+            if res[l]!=res[r]:
+                return False 
+            l+=1
+            r-=1
+        return True      
