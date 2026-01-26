6c7a605d4444980ba6c26dad0acaac2262788c28
Anudeep D V <anudeepdv333@gmail.com>
2026-01-25 19:19:33 -0600

    Time: 38 ms (7.29%) | Memory: 19.6 MB (16.28%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,27 +1,22 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        
         res=[]
         candidates.sort()
-        def dfs(start,s):
-            if sum(s)==target:
-                res.append(s[::])
-                return
-            if sum(s)>target:
-                return
-            if start >=len(candidates):
-                return
-            s.append(candidates[start])
-            dfs(start+1,s)
-            s.pop()
+        def dfs(start,cur,total):
 
-            while start+1<len(candidates) and candidates[start+1]==candidates[start]:
+            
+            if total==target:
+                res.append(cur.copy())
+                return
+
+            if start>=len(candidates) or total>target:
+                return
+
+            dfs(start+1,cur+[candidates[start]],total+candidates[start])
+            while start+1<len(candidates) and candidates[start]==candidates[start+1]:
                 start=start+1
 
-            dfs(start+1,s)
-
-        dfs(0,[])
+            dfs(start+1,cur,total)
+        dfs(0,[],0)
         return res
-            
-            
-
-        
